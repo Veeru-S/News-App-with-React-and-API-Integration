7cb64f1c4216da8456e672cdeb5ea7530d66e025
VEERU <veerugandegudi@gmail.com>
2024-06-29 22:59:40 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -51,7 +51,7 @@ export class News extends Component {
         </div>
         <div className="container d-flex justify-content-between">
         <button disabled={this.state.page<=1} type="button" class="btn btn-dark" onClick={this.handleprevclick}>&larr; Previous</button>
-        <button disabled={this.state.page + 1 > Math.cell{this.state.totalResults/5} type="button" class="btn btn-dark" onClick={this.handlenextclick}>Next &rarr;</button>
+        <button disabled={this.state.page + 1} > Math.cell{this.state.totalResults/5} type="button" class="btn btn-dark" onClick={this.handlenextclick}Next &rarr;</button>
         </div>
         <div className="container d-flex justify-content-center">
           <h6>Created by VEERU G</h6>
